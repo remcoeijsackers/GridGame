1525a1268c25472f4934da57324571cf904aacd6
Immortalsonly <remco.eijsackers@gmail.com>
2023-01-24 20:51:35 +0100

    cleanup

--- uibuilder/draw/painter.py
+++ uibuilder/draw/painter.py
@@ -7,8 +7,8 @@ from src.util import unit_thickness, symbol_thickness
 
 
 
-class painter:
-    def draw_tree(self, convert: convert_coords, canvas: tk.Canvas, symbol_size, logical_position):
+
+def draw_tree( convert: convert_coords, canvas: tk.Canvas, symbol_size, logical_position):
         logical_position = np.array(logical_position)
         grid_position = convert.convert_logical_to_grid_position(logical_position)
         canvas.create_oval(grid_position[0] - symbol_size, grid_position[1] - symbol_size,
@@ -21,7 +21,7 @@ class painter:
                                 grid_position[0] + symbol_size, grid_position[1] + symbol_size, width=symbol_thickness -30,
                                 outline=colorContext.green_color)
 
-    def draw_broken_cell(self, convert: convert_coords, canvas: tk.Canvas, symbol_size, logical_position):
+def draw_broken_cell(   convert: convert_coords, canvas: tk.Canvas, symbol_size, logical_position):
         grid_position = convert.convert_logical_to_grid_position(logical_position)
         canvas.create_line(grid_position[0] - symbol_size, grid_position[1] - symbol_size,
                                 grid_position[0] + symbol_size, grid_position[1] + symbol_size, width=symbol_thickness,
@@ -30,7 +30,7 @@ class painter:
                                 grid_position[0] + symbol_size, grid_position[1] - symbol_size, width=symbol_thickness,
                                 fill=colorContext.symbol_x_color)
 
-    def draw_building(self, convert: convert_coords, canvas: tk.Canvas, symbol_size, logical_position, color=colorContext.symbol_building_color):
+def draw_building(   convert: convert_coords, canvas: tk.Canvas, symbol_size, logical_position, color=colorContext.symbol_building_color):
         grid_position = convert.convert_logical_to_grid_position(logical_position)
         canvas.create_rectangle(grid_position[0], grid_position[1],
                                 grid_position[0], grid_position[1], width=symbol_thickness,
@@ -45,13 +45,13 @@ class painter:
                                 grid_position[1] + symbol_size, 
                                 fill=colorContext.canvas_text_color)
 
-    def draw_square(self, convert: convert_coords, canvas: tk.Canvas, logical_position, color):
+def draw_square(   convert: convert_coords, canvas: tk.Canvas, logical_position, color):
         grid_position = convert.convert_logical_to_grid_position(logical_position)
         canvas.create_rectangle(grid_position[0] , grid_position[1],
                                 grid_position[0], grid_position[1], width=symbol_thickness +10,
                                 fill=color, outline=color)
 
-    def draw_unit(self, convert: convert_coords, canvas: tk.Canvas, board, symbol_size, logical_position, color):
+def draw_unit(   convert: convert_coords, canvas: tk.Canvas, board, symbol_size, logical_position, color):
         grid_position = convert.convert_logical_to_grid_position(logical_position)
         mappos = convert.convert_logical_to_map(logical_position)
         health = board.gethealth(mappos)
@@ -83,7 +83,7 @@ class painter:
                                 grid_position[1] + symbol_size, 
                                 fill=colorContext.canvas_text_color, text=health)
     
-    def draw_dot(self, convert: convert_coords, canvas: tk.Canvas, logical_position, color, width=10):
+def draw_dot(   convert: convert_coords, canvas: tk.Canvas, logical_position, color, width=10):
         if color == colorContext.symbol_attack_dot_color or color == colorContext.gray_color:
             width = 20
         if color == colorContext.range_move_color:
